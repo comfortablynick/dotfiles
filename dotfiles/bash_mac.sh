35f683bcf962e08a57c4df88124dbda0059c98e4
Nick Murphy <comfortablynick@gmail.com>
2018-09-15 15:42:11 -0500

    Use virtualenvwrapper on mac

--- dotfiles/bash_mac.sh
+++ dotfiles/bash_mac.sh
@@ -5,14 +5,6 @@
 # Enable color support of ls and read color conifg file
 [ -f ${HOME}/.dircolors ] && eval $(gdircolors ${HOME}/.dircolors);
 
-# alias ls='ls --color=auto'
-# alias grep='grep --color=auto'
-# alias fgrep='fgrep --color=auto'
-# alias egrep='egrep --color=auto'
-
-# Load Git bash
-# [ -f ${HOME}/.git-bash-for-mac.sh ] && source ${HOME}/.git-bash-for-mac.sh;
-
 # Git
 alias gpython='cd ~/git/python'
 alias ggas='cd ~/git/google-apps-script/sheets'
@@ -22,13 +14,6 @@ alias gdspw='cd ~/git/google-apps-script/sheets/dspw'
 alias gdot='cd ~/dotfiles'
 alias gfst='cd ~/git/google-apps-script/sheets/fs-time'
 
-# Python Venv
-export VENV_DIR="${HOME}/env/bin/activate"
-alias py="source ${VENV_DIR}"
-
-# Activate venv by default
-source $VENV_DIR
-
 # Setting PATH for Python 3.6
 PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
 
@@ -37,6 +22,13 @@ PATH="/Library/Frameworks/Python.framework/Versions/3.7/bin:${PATH}"
 
 # Setting PATH for coreutils
 PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+
+# Python Venv
+export WORKON_HOME=${HOME}/.pyenv
+export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
+export VIRTUALENVWRAPPER_SCRIPT=/Library/Frameworks/Python.framework/Versions/3.7/bin/virtualenvwrapper.sh
+source /Library/Frameworks/Python.framework/Versions/3.7/bin/virtualenvwrapper.sh
+workon dev # Enter default venv
 
 # enable programmable completion features 
 if ! shopt -oq posix; then

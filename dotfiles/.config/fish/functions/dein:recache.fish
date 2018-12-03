ae8dadee0eff1a383ffb0732361f4418007780df
Nick Murphy <comfortablynick@gmail.com>
2018-12-03 13:24:43 -0600

    Delete dein functions

--- dotfiles/.config/fish/functions/dein:recache.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.qNOiTD/dein:recache.fish @ line 2
-function dein:recache --description 'recache dein plugin paths'
-	echo "try | call dein#recache_runtimepath() | finally | qall! | endtry"
-end
--- dotfiles/.config/fish/functions/dein:update.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.ATnyVM/dein:update.fish @ line 2
-function dein:update --description 'command for updating dein plugins'
-	echo "try | call dein#update() | finally | qall! | endtry"
-end

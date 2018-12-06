33129f4e63f9ed0cdd5204e8ad1b0f5cd185252b
Nick Murphy <comfortablynick@gmail.com>
2018-12-06 11:59:28 -0600

    Added todo.txt aliases; added folding

--- dotfiles/bash_aliases.sh
+++ dotfiles/bash_aliases.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
-# Add bash aliases here for all platforms
+# vim:fdm=marker:
 
-# Autocorrect
+# Add bash aliases here for all platforms
+# Autocorrect {{{1
 alias exif='exit'
 
-# ls
+# ls {{{1
 alias ls='ls -h --color=auto --group-directories-first'
 alias ll='ls -l'
 alias lla='ls -lA'
 alias la='ls -A'
 alias l='ls -CF'
 
-# grep
+# grep {{{1
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-# file handling
+# file handling {{{1
 alias rm='rm -I'
 alias mv='mv -i'
 alias cp='cp -i'
 alias rmdir='rm -rf'                    # Recursively remove dirs
 
-# navigation
+# navigation {{{1
 alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -31,7 +32,7 @@ alias h='cd ~/'
 alias r='cd /'
 alias q='exit'
 
-# Git
+# Git {{{1
 alias git='hub'                         # Use hub wrapper
 alias gpl='git pull'
 alias gph='git push'
@@ -43,17 +44,22 @@ alias gsub='git submodule foreach --recursive git pull origin master'
 alias guns='git reset HEAD'             # Unstage $file
 alias gri='git rm --cached'             # Remove from index but keep local copy of $file
 
-# dotdrop
+# dotdrop {{{1
 alias gdot='cd ~/dotfiles/dotfiles'
 alias dotdrop='~/dotfiles/dotdrop.sh'
 alias dotgit='git -C ~/dotfiles'
 alias dotsync="dotgit pull && dotgit add -A && dotgit commit && dotgit push; dotdrop install"
 
-# Vim / Neovim
-alias vdot='cd ~/dotfiles/dotfiles/.vim/config'
-# hash nvim &> /dev/null && alias vim='nvim' && alias v='nvim' || alias v='vim'     # Favor Neovim if it exists on this machine
-alias vim='nvim'
-alias v='nvim'
+# Todo.txt {{{1
+alias t='todo.sh'
+alias todo='todo.sh'
+alias tp='topydo'
 
-# xonsh
+# Vim / Neovim {{{1
+alias vdot='cd ~/dotfiles/dotfiles/.vim/config'
+hash nvim &> /dev/null && alias vim='nvim' && alias v='nvim' || alias v='vim'     # Favor Neovim if it exists on this machine
+# alias vim='nvim'
+# alias v='nvim'
+
+# xonsh {{{1
 alias xo='xonsh'

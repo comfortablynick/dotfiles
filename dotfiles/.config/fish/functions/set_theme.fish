aa08fbd1a065494473866b590c59f743cafdacee
Nick Murphy <comfortablynick@gmail.com>
2018-12-12 16:02:17 -0600

    Go back to writing theme to file rather than uvar

--- dotfiles/.config/fish/functions/set_theme.fish
+++ dotfiles/.config/fish/functions/set_theme.fish
@@ -1,20 +1,19 @@
-# Defined in /tmp/fish.LwmePr/set_theme.fish @ line 2
-function set_theme --description 'set local theme file and reload shell'
+# Defined in /tmp/fish.66uS5M/set_theme.fish @ line 2
+function set_theme --description 'set local theme file'
 	set -l options 's/ssh' 'h/help'
     set -l help_txt 'usage: set_theme NAME [ [--ssh] ]'
     argparse $options -- $argv
     test -z "$argv" && echo $help_txt && return 1
-
     set -q _flag_help && echo $help_txt && return 0
+
+    set -l file
     if set -q _flag_ssh
         echo "Setting ssh theme"
-        # Unset global variable (not needed)
-        set -eg FISH_SSH_THEME
-        set -Ux FISH_SSH_THEME $argv
+        set file "$XDG_DATA_HOME/fish/ssh_theme"
     else
         echo "Setting theme"
-        set -eg FISH_THEME
-        set -Ux FISH_THEME $argv
+        set file "$XDG_DATA_HOME/fish/theme"
     end
-    echo "Reload shell to see changes"
+    echo $argv > "$file"
+    and echo 'Reload shell to see changes'
 end

37e63cd436fc34c5acc9623c2bf9fd6427bd231c
Nick Murphy <comfortablynick@gmail.com>
2019-01-02 07:48:45 -0600

    Improve formatting; add env vars

--- dotfiles/.config/bash/conf.d/01_check_env.bash
+++ dotfiles/.config/bash/conf.d/01_check_env.bash
@@ -6,5 +6,5 @@ env_sha=$(sha1sum "$HOME/dotfiles/dotfiles/env.toml" | cut -d' ' -f1)
 if [ ! -f "$env_file" ] || [ "$env_sha" != "$(<env_file)" ]; then
     echo "Changes made to env.toml."
     parse_env.py $HOME/dotfiles/dotfiles/env.toml -b
-    echo "$env_sha" > "$env_file"
+    echo "$env_sha" >"$env_file"
 fi

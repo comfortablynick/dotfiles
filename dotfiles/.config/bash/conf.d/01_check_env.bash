0b93e473ef38ed97f2cf71f5eeb7e5d2498f1e4a
Nick Murphy <comfortablynick@gmail.com>
2019-01-03 08:40:18 -0600

    Add ls and sha1 shell functions

--- dotfiles/.config/bash/conf.d/01_check_env.bash
+++ dotfiles/.config/bash/conf.d/01_check_env.bash
@@ -1,7 +1,7 @@
 # Check env.toml checksum for changes
 
 env_file="$HOME/.env_toml_sha_bash"
-env_sha=$(sha1sum "$HOME/dotfiles/dotfiles/env.toml" | cut -d' ' -f1)
+env_sha=$(sha1 "$HOME/dotfiles/dotfiles/env.toml" | cut -d' ' -f1)
 
 if [ ! -f "$env_file" ] || [ "$env_sha" != "$(< $env_file)" ]; then
     echo "Changes made to env.toml since bash last started."

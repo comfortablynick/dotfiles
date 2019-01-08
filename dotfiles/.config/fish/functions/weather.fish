d4afacde37993f6d84f2e236a426d1224af6325b
Nick Murphy <comfortablynick@gmail.com>
2019-01-08 17:21:17 -0600

    Fix missing $argv

--- dotfiles/.config/fish/functions/weather.fish
+++ dotfiles/.config/fish/functions/weather.fish
@@ -1,8 +1,8 @@
-# Defined in - @ line 2
+# Defined in /tmp/fish.w1pfOR/weather.fish @ line 2
 function weather --description 'Display cli output of weather'
 	set -l script "$HOME/git/python/shell/weather/weather.py"
     if test -x $script
-        python $script
+        python $script $argv
     else if test -e $script
         echo "$script exists but is not executable."
     else

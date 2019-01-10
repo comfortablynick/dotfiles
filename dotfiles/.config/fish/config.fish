11ca400df172ab16cfbccd15202682380674219a
Nick Murphy <comfortablynick@gmail.com>
2019-01-10 12:06:44 -0600

    Don't activate virtual env, now that we're using pyenv

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -278,8 +278,8 @@ and set -g yimmy_solarized false
 
 # STARTUP COMMANDS {{{1
 # Python Venv {{{2
-test -d "$def_venv"
-and source $def_venv/bin/activate.fish
+# test -d "$def_venv"
+# and source $def_venv/bin/activate.fish
 
 # TMux {{{2
 # Attach to existing tmux or create a new session using custom function

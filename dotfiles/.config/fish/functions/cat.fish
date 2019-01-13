bae8881d2ecdb3d2b5c5b00930d84228df3ae00d
Nick Murphy <comfortablynick@gmail.com>
2019-01-13 10:08:17 -0600

    Fix missing $argv on cat wrapper

--- dotfiles/.config/fish/functions/cat.fish
+++ dotfiles/.config/fish/functions/cat.fish
@@ -1,12 +1,10 @@
-# Defined in /var/folders/gb/x1313fbd2klb5mss86_gsd1m0000gn/T//fish.uZucqZ/cat.fish @ line 2
+# Defined in /var/folders/gb/x1313fbd2klb5mss86_gsd1m0000gn/T//fish.o3v2T0/cat.fish @ line 2
 function cat --description 'wrapper for cat'
 	if type -q bat
         bat $argv
     else if type -q ccat
         ccat $argv
     else
-        # cat probably can't take the args
-        # so let's strip them for now
-        command cat
+        command cat $argv
     end
 end

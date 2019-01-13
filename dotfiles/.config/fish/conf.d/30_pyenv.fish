bb967e2778c487ce38d717acc3955cefe54d8262
Nick Murphy <comfortablynick@gmail.com>
2019-01-13 10:17:04 -0600

    Add virtualenv hook to conf.d dir

--- dotfiles/.config/fish/conf.d/30_pyenv.fish
+++
@@ -1,7 +0,0 @@
-# Source pyenv scripts
-if status is-interactive
-and test -n (command -v pyenv 2>/dev/null)
-    command pyenv rehash 2>/dev/null
-    # source (pyenv init -)
-    # source (pyenv virtualenv-init -)
-end
---
+++ dotfiles/.config/fish/conf.d/30_pyenv_virtualenv_hook.fish
@@ -0,0 +1,9 @@
+function _pyenv_virtualenv_hook --on-event fish_prompt;
+  set -l ret $status
+  if test -n "$VIRTUAL_ENV"
+    pyenv activate --quiet; or pyenv deactivate --quiet; or true
+  else
+    pyenv activate --quiet; or true
+  end
+  return $ret
+end

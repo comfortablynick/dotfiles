07d42d368619082aad8f595e830ca01471116a99
Nick Murphy <comfortablynick@gmail.com>
2019-01-21 10:06:18 -0600

    Adjustment to pyenv

--- dotfiles/.config/fish/conf.d/30_pyenv_virtualenv_hook.fish
+++ dotfiles/.config/fish/conf.d/30_pyenv_virtualenv_hook.fish
@@ -1,9 +1,14 @@
-function _pyenv_virtualenv_hook --on-event fish_prompt;
-  set -l ret $status
-  if test -n "$VIRTUAL_ENV"
-    pyenv activate --quiet; or pyenv deactivate --quiet; or true
-  else
-    pyenv activate --quiet; or true
-  end
-  return $ret
+test "$hostname" = "titan"
+and exit
+function _pyenv_virtualenv_hook --on-event fish_prompt
+    set -l ret $status
+    if test -n "$VIRTUAL_ENV"
+        pyenv activate --quiet
+        or pyenv deactivate --quiet
+        # or true
+    else
+        pyenv activate --quiet
+        # or true
+    end
+    return $ret
 end
--- dotfiles/.config/fish/functions/pyenv.fish
+++ dotfiles/.config/fish/functions/pyenv.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/gb/x1313fbd2klb5mss86_gsd1m0000gn/T//fish.b8ORbM/pyenv.fish @ line 2
+# Defined in /tmp/fish.1VAYav/pyenv.fish @ line 2
 function pyenv
 	set command $argv[1]
     set -e argv[1]
@@ -7,7 +7,6 @@ function pyenv
         case activate deactivate rehash shell
             source (command pyenv "sh-$command" $argv|psub)
         case '*'
-            command pyenv "$command" $argv
-            # command pyenv $command $argv
+            command pyenv $command $argv
     end
 end

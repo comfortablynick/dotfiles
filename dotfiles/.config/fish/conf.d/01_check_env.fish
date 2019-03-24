c2f4dac42e8d5a1644fdf1f7fbe80d5f9d7f0cf8
Nick Murphy <comfortablynick@gmail.com>
2019-03-24 12:33:33 -0500

    Add guard to only process env if not interactive

--- dotfiles/.config/fish/conf.d/01_check_env.fish
+++ dotfiles/.config/fish/conf.d/01_check_env.fish
@@ -1,19 +1,19 @@
 # Check env.toml file for changes compared with previous checksum
-set -l env_sha (string split ' ' (sha1 "$HOME/dotfiles/dotfiles/env.toml"))[1]
+if status is-interactive
+    set -l env_sha (string split ' ' (sha1 "$HOME/dotfiles/dotfiles/env.toml"))[1]
 
-if test "$env_sha" = "$env_toml_sha"
-    # No changes; we're good!
-    exit 0
+    if test "$env_sha" = "$env_toml_sha"
+        # No changes; we're good!
+        exit 0
+    end
+
+    # Run parsing script to update env.fish
+    echo "env.toml has changed!"
+    parse_env.py "$HOME/dotfiles/dotfiles/env.toml" -f
+
+    # Set variable to reload file on shell init
+    set -U env_file_sourced 0
+
+    # Update checksum
+    set -U env_toml_sha "$env_sha"
 end
-
-# Run parsing script to update env.fish
-echo "env.toml has changed!"
-# echo "Orig sha: $env_toml_sha"
-# echo "Curr sha: $env_sha"
-parse_env.py "$HOME/dotfiles/dotfiles/env.toml" -f
-
-# Set variable to reload file on shell init
-set -U env_file_sourced 0
-
-# Update checksum
-set -U env_toml_sha "$env_sha"

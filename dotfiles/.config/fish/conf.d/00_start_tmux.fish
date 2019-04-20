84cd59e1afd81d03a1ef8bf9a6b0c2d0c7744d10
Nick Murphy <comfortablynick@gmail.com>
2019-04-20 15:28:17 -0500

    Update tmux start

--- dotfiles/.config/fish/conf.d/00_start_tmux.fish
+++ dotfiles/.config/fish/conf.d/00_start_tmux.fish
@@ -1,31 +1,34 @@
 # Start tmux session for interactive shells
 if status is-interactive
-    and test -n (type -f tmux 2>/dev/null)
-    and test -z "$TMUX"
-    and not set -q no_tmux_login
-    if not set -q no_tmux_next_login
-        if test $COLUMNS -gt 200
-            set -gx SEP ''
-            set -gx SUB ''
-            set -gx RSEP ''
-            set -gx RSUB ''
-        else
-            set -gx SEP ''
-            set -gx SUB '|'
-            set -gx RSEP ''
-            set -gx RSUB '|'
-        end
-        test -n "$SSH_CONNECTION"
-        and set -l session_name 'ios'
-        or set -l session_name 'def'
-
-        exec tmux -2 new-session -A -s "$session_name"
+    if test $COLUMNS -gt 200
+        set -gx SEP ''
+        set -gx SUB ''
+        set -gx RSEP ''
+        set -gx RSUB ''
     else
-        set -e no_tmux_next_login
-        set_color yellow
-        echo "Note: 'no_tmux_next_login' flag was set for this login."
-        echo "TMUX will be used on next login unless flag is reset."
-        echo ""
-        set_color normal
+        set -gx SEP ''
+        set -gx SUB '|'
+        set -gx RSEP ''
+        set -gx RSUB '|'
+    end
+    if test -n (type -f tmux 2>/dev/null)
+        and test -z "$TMUX"
+        and not set -q no_tmux_login
+        begin
+            if not set -q no_tmux_next_login
+                test -n "$SSH_CONNECTION"
+                and set -l session_name 'ios'
+                or set -l session_name 'ios'
+
+                exec tmux -2 new-session -A -s "$session_name"
+            else
+                set -e no_tmux_next_login
+                set_color yellow
+                echo "Note: 'no_tmux_next_login' flag was set for this login."
+                echo "TMUX will be used on next login unless flag is reset."
+                echo ""
+                set_color normal
+            end
+        end
     end
 end
--- dotfiles/bash_mac.sh
+++ dotfiles/bash_mac.sh
@@ -6,19 +6,19 @@
 [ -f ${HOME}/.dircolors ] && eval $(gdircolors ${HOME}/.dircolors);
 
 # Git
-alias gpython='cd ~/git/python'
-alias ggas='cd ~/git/google-apps-script/sheets'
-alias gcp='cd ~/git/google-apps-script/sheets/convention-personnel'
-alias gcpr='cd ~/git/google-apps-script/sheets/convention-personnel-reports'
-alias gdspw='cd ~/git/google-apps-script/sheets/dspw'
-alias gdspwe='cd ~/git/google-apps-script/sheets/dspw-email'
-alias gfst='cd ~/git/google-apps-script/sheets/fs-time'
+# alias gpython='cd ~/git/python'
+# alias ggas='cd ~/git/google-apps-script/sheets'
+# alias gcp='cd ~/git/google-apps-script/sheets/convention-personnel'
+# alias gcpr='cd ~/git/google-apps-script/sheets/convention-personnel-reports'
+# alias gdspw='cd ~/git/google-apps-script/sheets/dspw'
+# alias gdspwe='cd ~/git/google-apps-script/sheets/dspw-email'
+# alias gfst='cd ~/git/google-apps-script/sheets/fs-time'
 
 # Setting PATH for Python 3.6
-PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+# PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
 
 # Setting PATH for Python 3.7
-PATH="/Library/Frameworks/Python.framework/Versions/3.7/bin:${PATH}"
+# PATH="/Library/Frameworks/Python.framework/Versions/3.7/bin:${PATH}"
 
 # Setting PATH for coreutils
 PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
@@ -28,12 +28,12 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Python Venv
-export VENV_DIR="${HOME}/.env"
-export NVIM_PY2_DIR="${VENV_DIR}/nvim2"
-export NVIM_PY3_DIR="${VENV_DIR}/nvim3"
+# export VENV_DIR="${HOME}/.env"
+# export NVIM_PY2_DIR="${VENV_DIR}/nvim2"
+# export NVIM_PY3_DIR="${VENV_DIR}/nvim3"
 
-alias denv="source ${VENV_DIR}/dev/bin/activate"
-. ${VENV_DIR}/dev/bin/activate
+# alias denv="source ${VENV_DIR}/dev/bin/activate"
+# . ${VENV_DIR}/dev/bin/activate
 
 # enable programmable completion features 
 if ! shopt -oq posix; then
@@ -45,7 +45,7 @@ if ! shopt -oq posix; then
 fi
 
 # Powerline
-export POWERLINE_ROOT="/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/powerline"
+# export POWERLINE_ROOT="/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/powerline"
 
 #--------------------------------------------------------------------------
 # MAC FUNCTIONS

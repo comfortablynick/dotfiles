397b94e5bcba7ce0ce397b3f257d759b3125352f
Nick Murphy <comfortablynick@gmail.com>
2019-08-12 10:14:48 -0500

    Remove pyenv and use default python3 interpreter

--- dotfiles/.config/zsh/conf.d/01_check_env.zsh
+++ dotfiles/.config/zsh/conf.d/01_check_env.zsh
@@ -1,13 +1,12 @@
 # Check env.toml checksum for changes
+# Python must have tomlkit installed
 
 env_file="$HOME/.env_toml_sha_zsh"
 sha_cmd=$(command -v sha1sum || command -v gsha1sum)
-env_sha=$(eval "$sha_cmd" "$HOME/dotfiles/dotfiles/env.toml" | cut -d' ' -f1)
+env_sha="$(eval $sha_cmd $HOME/dotfiles/dotfiles/env.toml | cut -d' ' -f1)"
 
-if [ ! -f "$env_file" ] || [ "$env_sha" != "$(<$env_file)" ]; then
+if [[ ! -f $env_file ]] || [[ $env_sha != $(<$env_file) ]]; then
     echo "Changes made to env.toml since zsh last started."
-    pyenv_ver="$(head -n 1 $HOME/.pyenv/version)"
-    py_path="$HOME/.pyenv/versions/$pyenv_ver/bin/python"
-    "$py_path" "$HOME/.config/shell/functions/parse_env" -z "$HOME/dotfiles/dotfiles/env.toml"
+    python3 "$HOME/.config/shell/functions/parse_env" -z "$HOME/dotfiles/dotfiles/env.toml"
     echo "$env_sha" >"$env_file"
 fi

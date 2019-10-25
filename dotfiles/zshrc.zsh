7d33939dc9e0dbe8c58a5c4a79c8af0b3e5dd260
Nick Murphy <comfortablynick@gmail.com>
2019-10-25 09:54:47 -0500

    Add alias to remove last history zsh

--- dotfiles/zshrc.zsh
+++ dotfiles/zshrc.zsh
@@ -291,6 +291,45 @@ if [[ -e $HOME/.asdf/asdf.sh ]]; then
 fi
 # END ANSIBLE MANAGED BLOCK: asdf
 
+# remove_last_history_entry :: delete last `n` commands from history {{{2
+remove_last_history_entry() {
+    # This sub-function checks if the argument passed is a number.
+    # Thanks to @yabt on stackoverflow for this :).
+    is_int() ( return $(test "$@" -eq "$@" > /dev/null 2>&1); )
+
+    # Set history file's location
+    history_file="${HOME}/.zsh_history"
+    history_temp_file="${history_file}.tmp"
+    line_cout=$(wc -l $history_file)
+
+    # Check if the user passed a number,
+    # so we can delete x lines from history.
+    lines_to_remove=1
+    if [ $# -eq 0 ]; then
+        # No arguments supplied, so set to one.
+        lines_to_remove=1
+    else
+        # An argument passed. Check if it's a number.
+        if $(is_int "${1}"); then
+            lines_to_remove="$1"
+        else
+            echo "Unknown argument passed. Exiting..."
+            return
+        fi
+    fi
+
+    # Make the number negative, since head -n needs to be negative.
+    lines_to_remove="-${lines_to_remove}"
+
+    fc -W # write current shell's history to the history file.
+
+    # Get the files contents minus the last entry(head -n -1 does that)
+    cat $history_file | head -n "${lines_to_remove}" &> $history_temp_file
+    mv "$history_temp_file" "$history_file" > /dev/null 2>&1
+
+    fc -R # read history file.
+}
+
 # SHELL STARTUP {{{1
 # Debug end {{{2
 [[ $DEBUG_MODE = true ]] && echo "Exiting .zshrc"

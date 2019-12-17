b26db3c26605b9b49e9bdeb3105de9fc3ee30c81
Nick Murphy <comfortablynick@gmail.com>
2019-12-17 14:40:21 -0600

    Move general mappings to init.lua

--- dotfiles/.vim/lua/init.lua
+++ dotfiles/.vim/lua/init.lua
@@ -178,9 +178,29 @@ local global_vars = {
     -- Filetypes that will use a completion plugin
     completion_filetypes = {
         coc = {
-            "c", "cpp", "fish", "go", "rust", "typescript", "javascript",
-            "json", "lua", "python", "bash", "sh", "vim", "yaml", "snippets",
-            "markdown", "toml", "txt", "mail", "pro", "ini", "muttrc", "cmake",
+            "c",
+            "cpp",
+            "fish",
+            "go",
+            "rust",
+            "typescript",
+            "javascript",
+            "json",
+            "lua",
+            "python",
+            "bash",
+            "sh",
+            "vim",
+            "yaml",
+            "snippets",
+            "markdown",
+            "toml",
+            "txt",
+            "mail",
+            "pro",
+            "ini",
+            "muttrc",
+            "cmake",
         },
         ["nvim-lsp"] = {},
     },
@@ -229,6 +249,52 @@ local mappings = {
     ["n<CR>"] = {":noh<CR><CR>", silent = false},
     -- Show syntax group under cursor
     ["n<Leader>h"] = {":echo syntax#syn_group()<CR>"},
+    -- Shortcuts to open files
+    ["n<Leader>il"] = {
+        (":vsplit %s<CR>"):format(
+            vim.env.XDG_DATA_HOME .. "/nvim/site/lua/init.lua"
+        ),
+    },
+    ["n<Leader>iv"] = {
+        (":vsplit %s<CR>"):format(vim.env.XDG_CONFIG_HOME .. "/nvim/init.vim"),
+    },
+    -- Navigation
+    -- Line navigation in insert mode
+    ["i<C-k>"] = {"<Up>"},
+    ["i<C-j>"] = {"<Down>"},
+    ["i<C-h>"] = {"<Left>"},
+    ["i<C-l>"] = {"<Right>"},
+    -- CTRL+{h,j,k,l} to navigate in normal mode
+    -- Likely overridden by vim-tmux-navigator
+    ["n<C-k>"] = {"<C-w><C-k>"},
+    ["n<C-j>"] = {"<C-w><C-j>"},
+    ["n<C-h>"] = {"<C-w><C-h>"},
+    ["n<C-l>"] = {"<C-w><C-l>"},
+    -- ALT+{h,j,k,l} to navigate from any mode
+    -- Terminal
+    ["t<A-k>"] = {[[<C-\><C-N><C-w>k]]},
+    ["t<A-j>"] = {[[<C-\><C-N><C-w>j]]},
+    ["t<A-h>"] = {[[<C-\><C-N><C-w>h]]},
+    ["t<A-l>"] = {[[<C-\><C-N><C-w>l]]},
+    -- Insert
+    ["i<A-k>"] = {[[<C-\><C-N><C-w>k]]},
+    ["i<A-j>"] = {[[<C-\><C-N><C-w>j]]},
+    ["i<A-h>"] = {[[<C-\><C-N><C-w>h]]},
+    ["i<A-l>"] = {[[<C-\><C-N><C-w>l]]},
+    -- Normal
+    ["n<A-k>"] = {"<C-w>k"},
+    ["n<A-j>"] = {"<C-w>j"},
+    ["n<A-h>"] = {"<C-w>h"},
+    ["n<A-l>"] = {"<C-w>l"},
+    -- Delete window with d<C-h,j,k,l>
+    ["ndk"] = {"<C-w>k<C-w>c", silent = false},
+    ["ndj"] = {"<C-w>j<C-w>c"},
+    ["ndh"] = {"<C-w>h<C-w>c"},
+    ["ndl"] = {"<C-w>l<C-w>c", silent = false},
+    -- t + {h,l,n} to navigate tabs
+    ["nth"] = {":tabprev<CR>"},
+    ["ntl"] = {":tabnext<CR>"},
+    ["ntn"] = {":tabnew<CR>"},
 }
 
 -- Test: define plugins for vim-packager {{{1

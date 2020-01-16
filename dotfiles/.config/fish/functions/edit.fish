0b4a5eed795a242b934a7692d927896d75618d1a
Nick Murphy <comfortablynick@gmail.com>
2020-01-16 14:38:49 -0600

    Add logic to pick fuzzy finder from available

--- dotfiles/.config/fish/functions/edit.fish
+++ dotfiles/.config/fish/functions/edit.fish
@@ -1,6 +1,16 @@
-# Defined in /tmp/fish.FB4Tg0/edit.fish @ line 2
+# Defined in /tmp/fish.KzvhkJ/edit.fish @ line 2
 function edit
-    eval "$FZY_DEFAULT_COMMAND" | fzy | read file
+    set -l fuzzy_finders rff fzy fzf sk
+    # Use the first executable fuzzy finder
+    for finder in $fuzzy_finders
+        if type -qf $finder
+            break
+        end
+    end
+    if test -z "$finder"
+        echo "Missing fuzzy finder; install fzy, fzf, etc. before running this command"
+    end
+    eval "$FZY_DEFAULT_COMMAND" | $finder | read file
     if test -z "$file"
         commandline -f repaint
         return

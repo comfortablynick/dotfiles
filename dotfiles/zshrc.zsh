82c11c2499d8f97fe567d22c9a24c645542c6cdb
Nick Murphy <comfortablynick@gmail.com>
2020-01-31 17:29:05 -0600

    Add funcs to zsh autoload dir

--- dotfiles/zshrc.zsh
+++ dotfiles/zshrc.zsh
@@ -139,14 +139,15 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
-
 bindkey -M viins "kj" vi-cmd-mode                               # Add `kj` -> ESC
 
-autoload -Uz edit-command-line
+autoload -Uz edit-command-line                                  # ! in vi cmd mode opens prompt in $EDITOR
 zle -N edit-command-line
-
 bindkey -M vicmd '!' edit-command-line
-bindkey '^E' fzy-file-widget
+
+autoload -Uz fzy-edit
+zle -N fzy-edit
+bindkey '^E' fzy-edit
 
 # PLUGINS {{{1
 # Zplugin Config {{{2
@@ -311,59 +312,8 @@ if [[ -e $HOME/.asdf/asdf.sh ]]; then
 fi
 # END ANSIBLE MANAGED BLOCK: asdf
 
-# remove_last_history_entry :: delete last `n` commands from history {{{2
-remove_last_history_entry() {
-    # This sub-function checks if the argument passed is a number.
-    # Thanks to @yabt on stackoverflow for this :).
-    is_int() ( return $(test "$@" -eq "$@" > /dev/null 2>&1); )
-
-    # Set history file's location
-    history_file="${HOME}/.zsh_history"
-    history_temp_file="${history_file}.tmp"
-    line_cout=$(wc -l $history_file)
-
-    # Check if the user passed a number,
-    # so we can delete x lines from history.
-    lines_to_remove=1
-    if [ $# -eq 0 ]; then
-        # No arguments supplied, so set to one.
-        lines_to_remove=1
-    else
-        # An argument passed. Check if it's a number.
-        if $(is_int "${1}"); then
-            lines_to_remove="$1"
-        else
-            echo "Unknown argument passed. Exiting..."
-            return
-        fi
-    fi
-
-    # Make the number negative, since head -n needs to be negative.
-    lines_to_remove="-${lines_to_remove}"
-
-    fc -W # write current shell's history to the history file.
-
-    # Get the files contents minus the last entry(head -n -1 does that)
-    cat $history_file | head -n "${lines_to_remove}" &> $history_temp_file
-    mv "$history_temp_file" "$history_file" > /dev/null 2>&1
-
-    fc -R # read history file.
-}
-
-_fzy-edit() {
-    # zle kill-whole-line
-    local file=$(eval "$FZY_DEFAULT_COMMAND" | fzy)
-    # zle -U "$EDITOR $file"
-    # zle accept-line
-    echo -n "$file"
-    echo
-}
-zle -N _fzy-edit
-
-# e :: fuzzy find file and edit in $EDITOR
-ed() {
-    eval "$FZY_DEFAULT_COMMAND" | fzy | xargs -r "$EDITOR" $file
-}
+# Autoload functions {{{2
+autoload -Uz remove_last_history_entry
 
 # SHELL STARTUP {{{1
 # Debug end {{{2

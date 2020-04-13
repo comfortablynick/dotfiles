3a120528e94e5f362791a7290d5eaca7b7462aec
Nick Murphy <comfortablynick@gmail.com>
2020-04-13 13:20:08 -0500

    Correct FZF_TAB_COMMAND and handle it in zshrc for now

--- zshrc.zsh
+++ zshrc.zsh
@@ -199,20 +199,19 @@ zinit load junegunn/fzf
 
 # Set this here to configure via zstyle
 # https://github.com/Aloxaf/fzf-tab/issues/73#issuecomment-610826492
-export FZF_TAB_COMMAND=(
+FZF_TAB_COMMAND=(
     fzf-tmux
     --ansi   # Enable ANSI color support, necessary for showing groups
     --expect='$continuous_trigger' # For continuous completion
     '--color=hl:$(( $#headers == 0 ? 108 : 255 ))'
     --nth=2,3 --delimiter='\x00'  # Don't search prefix
-    --layout=reverse --height=${FZF_TMUX_HEIGHT:=75%}
+    --layout=reverse --height=100
     --tiebreak=begin -m --bind=tab:down,btab:up,change:top,ctrl-space:toggle --cycle
     '--query=$query'   # $query will be expanded to query string at runtime.
     '--header-lines=$#headers' # $#headers will be expanded to lines of headers at runtime
 )
-zinit ice wait'1' lucid \
-    atload'zstyle ":fzf-tab:*" command $FZF_TAB_COMMAND'
-    zinit light Aloxaf/fzf-tab
+zinit ice wait"1" atload'zstyle ":fzf-tab:*" command $FZF_TAB_COMMAND' lucid
+zinit light Aloxaf/fzf-tab
 
 # dircolors {{{2
 # Use my fork of trapd00r plugin

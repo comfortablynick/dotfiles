cf121e67dd9d699542f689fbaa7990f2d76baec5
Nick Murphy <comfortablynick@gmail.com>
2020-06-23 16:57:39 -0500

    Allow icons if not on mosh connection

--- .config/fish/functions/d.fish
+++ .config/fish/functions/d.fish
@@ -1,3 +1,8 @@
+# Defined in /tmp/fish.7U1VwY/d.fish @ line 2
 function d --description 'alias for lsd with settings'
-	command lsd -alF --icon=never --group-dirs=first --date=relative
+    set -l cmd "command lsd -alF --group-dirs first --date relative"
+    if test $MOSH_CONNECTION -eq 1
+        set cmd "$cmd --icon never"
+    end
+    eval $cmd
 end

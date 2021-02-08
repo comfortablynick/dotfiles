1e1171bc61037f6b3591e4c490559b4d1a9d2cda
Nick Murphy <comfortablynick@gmail.com>
2021-02-08 10:33:22 -0600

    Use -A for almost all

--- .config/fish/functions/d.fish
+++ .config/fish/functions/d.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.FxzgrS/d.fish @ line 2
+# Defined in /tmp/fish.8Rcfsw/d.fish @ line 2
 function d --description 'alias for lsd with settings'
-    set -l cmd "command lsd -alF --group-dirs first --date relative"
+    set -l cmd "command lsd -AlF --group-dirs first --date relative"
     if test $MOSH_CONNECTION -eq 1
         set cmd "$cmd --icon never"
     end

8728dfaeb447cff7f90b9ab3b25e2de045e71c4b
Nick Murphy <comfortablynick@gmail.com>
2021-03-05 16:28:18 -0600

    Update asdf default versions

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,14 +1,4 @@
-# Defined in /tmp/fish.zw6ahS/git.fish @ line 2
+# Defined in /tmp/fish.21s9UF/git.fish @ line 2
 function git --description 'alias git=hub'
-    if builtin pwd -P | string match -qr "^$HOME"
-        and not command git rev-parse --show-toplevel &>/dev/null
-
-        set_color blue --bold
-        echo 'NOTE: using yadm git'
-        set_color normal
-
-        yadm $argv
-        return
-    end
     hub $argv
 end

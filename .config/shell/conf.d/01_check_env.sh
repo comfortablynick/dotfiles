bb408668cef3748ade0d9446b86a768f1655ba8b
Nick Murphy <comfortablynick@gmail.com>
2021-03-16 10:44:28 -0500

    Location of env.toml moved

--- .config/shell/conf.d/01_check_env.sh
+++ .config/shell/conf.d/01_check_env.sh
@@ -3,11 +3,12 @@
 # Check env.toml checksum for changes
 # Python must have tomlkit installed
 env_file="$HOME/.env_toml_sha_$CURRENT_SHELL"
+env_toml_file="$HOME/env.toml"
 sha_cmd="$(command -v sha1sum || command -v gsha1sum)"
-env_sha="$(eval "$sha_cmd" "$HOME/dotfiles/env.toml" | cut -d' ' -f1)"
+env_sha="$(eval "$sha_cmd" "$env_toml_file" | cut -d' ' -f1)"
 
 if [ ! -f "$env_file" ] || [ "$env_sha" != "$(cat "$env_file")" ]; then
     echo "Changes made to env.toml since $CURRENT_SHELL last started."
-    "$HOME/.asdf/bin/asdf" exec python3 "$HOME/bin/parse_env" "$HOME/dotfiles/env.toml" "$XDG_CONFIG_HOME/$CURRENT_SHELL/conf.d/01_env.$CURRENT_SHELL" -s "$CURRENT_SHELL"
+    "$HOME/.asdf/bin/asdf" exec python3 "$HOME/bin/parse_env" "$env_toml_file" "$XDG_CONFIG_HOME/$CURRENT_SHELL/conf.d/01_env.$CURRENT_SHELL" -s "$CURRENT_SHELL"
     echo "$env_sha" >"$env_file"
 fi

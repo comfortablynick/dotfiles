850c5196869de557b096f8ccdb53fdaf2f7cadb8
Nick Murphy <comfortablynick@gmail.com>
2021-04-01 17:33:13 -0500

    Add ansible lsd role

--- .config/nvim/lua/config/lsp/yamlls.lua
+++ .config/nvim/lua/config/lsp/yamlls.lua
@@ -6,15 +6,17 @@ return function(on_attach)
     on_attach = on_attach,
     filetypes = {"yaml", "yaml.ansible"},
     root_dir = function(fname)
-      return util.root_pattern(".git", ".yamllint", ".projections.json")(fname) or
-               util.find_git_ancestor(fname) or util.path.dirname(fname)
+      return
+        util.root_pattern(".git", ".yamllint", ".projections.json")(fname) or
+          util.find_git_ancestor(fname) or util.path.dirname(fname)
     end,
     settings = {
       yaml = {
         format = {enable = true, printWidth = 100},
         schemas = {
-          ["https://json.schemastore.org/ansible-playbook"] = ".ansible/playbooks/*.yml",
-          ["https://json.schemastore.org/ansible-role-2.9"] = ".ansible/roles/*/tasks/*.yml",
+          -- ["https://json.schemastore.org/ansible-role-2.9"] = "tasks/**/*.yml",
+          ["https://json.schemastore.org/ansible-role-2.9.json"] = "**/roles/tasks/**/*.yml",
+          ["https://json.schemastore.org/ansible-playbook.json"] = ".ansible/playbooks/*.yml",
           ["https://gist.githubusercontent.com/KROSF/c5435acf590acd632f71bb720f685895/raw/6f11aa982ad09a341e20fa7f4beed1a1b2a8f40e/taskfile.schema.json"] = "Taskfile.yml", -- luacheck: no max line length
         },
       },

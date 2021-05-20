8ddbf0c0bfae91667eae6adc9e1bf62989e94171
Nick Murphy <comfortablynick@gmail.com>
2021-05-19 20:32:48 -0500

    Removed comment

--- .config/fish/functions/gv.fish
+++ .config/fish/functions/gv.fish
@@ -1,4 +1,3 @@
-# Defined in /home/nick/.config/fish/env.fish @ line 178
 function gv --description 'alias view current repo using gv.vim'
  $EDITOR +"packadd vim-fugitive" +"packadd gv.vim" +GV +"autocmd BufWipeout <buffer> qall"; 
 end

ba85fd8f1bfece153dd70230297890985c24f809
Nick Murphy <comfortablynick@gmail.com>
2021-06-14 16:49:23 -0500

    Nuisance commit for changed comment

--- .config/fish/functions/gv.fish
+++ .config/fish/functions/gv.fish
@@ -1,3 +1,4 @@
+# Defined in /home/nick/.config/fish/env.fish @ line 177
 function gv --description 'alias view current repo using gv.vim'
  $EDITOR +"packadd vim-fugitive" +"packadd gv.vim" +GV +"autocmd BufWipeout <buffer> qall"; 
 end

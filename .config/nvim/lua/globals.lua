3da0361e7465655434ce3fe2f2cfd4642137767b
Nick Murphy <comfortablynick@gmail.com>
2021-07-07 15:00:36 -0500

    Simplify check_back_space

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -22,11 +22,7 @@ end
 -- TODO: possibly include markdown tab logic
 local check_back_space = function()
   local col = vim.fn.col(".") - 1
-  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-    return true
-  else
-    return false
-  end
+  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
 end
 
 -- Use (s-)tab to:

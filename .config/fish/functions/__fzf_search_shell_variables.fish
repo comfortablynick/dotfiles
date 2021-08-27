95b9c04090cc8a0bf043c701e8808c1bed50ace5
Nick Murphy <comfortablynick@gmail.com>
2021-08-27 13:07:19 -0500

    Use fzf-tmux

--- .config/fish/functions/__fzf_search_shell_variables.fish
+++ .config/fish/functions/__fzf_search_shell_variables.fish
@@ -1,8 +1,8 @@
-function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+function __fzf_search_shell_variables --description 'Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor.'
     # Make sure that fzf uses fish to execute __echo_value_or_print_message, which
     # is an autoloaded fish function so doesn't exist in other shells.
     # Using --local so that it does not clobber SHELL outside of this function.
-    set --local --export SHELL (command --search fish)
+    set -lx SHELL (command -s fish)
 
     # Pipe the names of all shell variables to fzf and attempt to display the value
     # of the selected variable in fzf's preview window.
@@ -10,12 +10,12 @@ function __fzf_search_shell_variables --description "Search and inspect shell va
     # __echo_value_or_print_message will print an informative message in lieu of the value.
     set variable_name (
         set --names |
-        fzf --preview '__fzf_display_value_or_error {}'
+        fzf-tmux --preview '__fzf_display_value_or_error {}'
     )
 
     if test $status -eq 0
-        commandline --insert $variable_name
+        commandline -i $variable_name
     end
 
-    commandline --function repaint
+    commandline -f repaint
 end

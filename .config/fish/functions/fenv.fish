15dfeb69a2417ac0f3d18f0c632f246fd417a89a
Nick Murphy <comfortablynick@gmail.com>
2021-08-27 13:06:49 -0500

    Delete unneeded fish functions

--- .config/fish/functions/fenv.fish
+++
@@ -1,8 +0,0 @@
-# Defined in /tmp/fish.27y1eg/fenv.fish @ line 2
-function fenv --description 'fuzzy search shell env'
-    # set -l cmd "env -0 | fzf --read0"
-    # if test -n "$argv"
-    #     set -a cmd "--prompt $argv"
-    # end
-    env -0 | fzf --read0 --prompt='ENV:> ' --query="$argv"
-end
--- .config/fish/functions/list.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.KosLJr/list.fish @ line 2
-function list --description 'Python implementation of directory listing'
-	list.py $argv
-end

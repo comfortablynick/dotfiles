0131145e0c94f5b3376c2bfa3a67c0999983a7d5
Nick Murphy <comfortablynick@gmail.com>
2021-10-18 12:03:13 -0500

    Improve root dir detection

--- .config/nvim/lua/config/lsp/server/pyright.lua
+++ .config/nvim/lua/config/lsp/server/pyright.lua
@@ -1,7 +1,13 @@
+local util = require "lspconfig/util"
 return function(on_attach)
   return {
     cmd = { "pyright-langserver", "--stdio" },
     on_attach = on_attach,
     filetypes = { "python" },
+    root_dir = function(fname)
+      return util.root_pattern(".git", "__pycache__", "setup.cfg")(fname)
+        or util.find_git_ancestor(fname)
+        or util.path.dirname(fname)
+    end,
   }
 end

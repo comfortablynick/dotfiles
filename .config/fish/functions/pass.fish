67e0138748ff3a19f49e2b3ed6ce65fc3e4d31f8
Nick Murphy <comfortablynick@gmail.com>
2021-12-08 10:49:28 -0600

    Updated to quit if lpass not logged in

--- .config/fish/functions/pass.fish
+++ .config/fish/functions/pass.fish
@@ -1,18 +1,23 @@
 function pass --description 'fuzzy find passwords from lastpass-cli'
     argparse h/help p/password -- $argv; or return
     if set -q _flag_help
-        echo 'Fuzzy find passwords from lastpass-cli'
-        echo
-        echo 'Usage: pass [{-h|--help}{-p|--password}] PATTERN'
-        echo
-        echo 'PATTERN    Search string to pass to `lpass ls`'
-        echo
-        echo 'Flags:'
-        echo '  -h, --help          Show this help message and exit'
-        echo '  -p, --password      Output password only to stdout'
+        echo "\
+Fuzzy find passwords from lastpass-cli
+
+Usage: pass [{-h|--help}{-p|--password}] PATTERN
+
+PATTERN    Search string to pass to `lpass ls`
+
+Flags:
+  -h, --help          Show this help message and exit
+  -p, --password      Output password only to stdout\
+" 1>&2
         return 1
     end
-
+    if not lpass status -q
+        echo (set_color brred)'error: '(set_color -o brblue)'must log in to lpass cli first'(set_color normal) 1>&2
+        return 1
+    end
     lpass ls | fzf-tmux -1 -q "$argv" | read -l result
     if test -z "$result"
         return 1

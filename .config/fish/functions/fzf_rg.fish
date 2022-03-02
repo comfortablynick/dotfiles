9eb4e840e8cc9877e3ed68aa159587f6b70e9c78
Nick Murphy <comfortablynick@gmail.com>
2022-03-02 17:01:07 -0600

    Improve ansible config and add uranus

--- .config/fish/functions/fzf_rg.fish
+++ .config/fish/functions/fzf_rg.fish
@@ -1,9 +1,9 @@
 function fzf_rg --description 'Search files using rg + fzf and open in nvim'
-    set -l initial_query (string escape "$argv")
+    set -l initial_query (string escape -- "$argv")
     set -l rg_cmd "rg --vimgrep --color=always --smart-case"
     set -l change_key ctrl-f
 
-    FZF_DEFAULT_COMMAND="$rg_cmd $initial_query" fzf-tmux -p 60%,80% --ansi \
+    FZF_DEFAULT_COMMAND="$rg_cmd $initial_query" fzf-tmux -p 80%,80% --ansi \
         --color "hl:-1:underline,hl+:-1:underline:reverse" \
         --disabled --query "$argv" \
         --bind "change:reload:sleep 0.1; $rg_cmd {q} || true" \
@@ -13,17 +13,16 @@ function fzf_rg --description 'Search files using rg + fzf and open in nvim'
         --delimiter : \
         --preview 'bat --color=always {1} --highlight-line {2}' \
         --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' | read -l result
-    set -l files (string split : $result)
+    set -l files (string split : -- $result)
 
     if test -z $files[1]
         commandline -f repaint
         return
     end
 
-    set -l cursor (string escape "norm!$files[2]G$files[3]|")
+    set -l cursor (string escape -- "norm!$files[2]G$files[3]|")
     # Start commandline with space so that we can re-use command right away
     # but it will not persist in history
-    commandline -r " "
-    commandline -it -- "nvim $files[1] +$cursor"
+    commandline -r -- " nvim $files[1] +$cursor"
     commandline -f execute
 end

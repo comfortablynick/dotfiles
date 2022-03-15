82789d7797311c224089af6fc0e1a1a28bec3aea
Nick Murphy <comfortablynick@gmail.com>
2022-03-14 23:53:27 -0500

    No need for eval on cat.fish

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,7 +1,7 @@
 function cat --description 'alias cat for best cat option installed'
     for cat in bat ccat gcat cat
         if command -q $cat
-            eval command $cat $argv
+            command $cat $argv
             return
         end
     end

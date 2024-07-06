3050b23b8cefcd5179d9b7caae991c0056eb3846
Nick Murphy <comfortablynick@gmail.com>
2024-07-05 21:17:45 -0700

    Use `eza` for d alias instead of lsd

--- .config/fish/functions/eza.fish
+++ .config/fish/functions/eza.fish
@@ -1,3 +1,3 @@
 function eza --description 'Run eza with defaults' --wraps eza
-    command eza -la --group-directories-first --icons --color=always $argv | bat -p
+    command eza -la --group-directories-first --icons --color-scale $argv
 end
